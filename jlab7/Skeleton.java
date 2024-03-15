6485cc520eb21e717d246b0255e61369c7e2acc3
maxg1807 <159554299+maxg1807@users.noreply.github.com>
2024-03-15 14:44:49 +0000

    Add files via upload

---
+++ jlab7/Skeleton.java
@@ -0,0 +1,20 @@
+import.util.Scanner;
+
+class Jlab71
+
+
+{
+    public static void main(String[] args)
+    {
+		
+    }
+    static boolean isEven(int input)
+        {
+        boolean boo = false;
+        if(input % 2 ==0)
+                {
+                    boo = true;
+                }
+          return boo;
+        }
+}
---
+++ jlab7/jlab71.java
@@ -0,0 +1,43 @@
+import java.util.Scanner;
+
+class Jlab71
+
+
+{
+    public static void main(String[] args)
+    {
+		Scanner scan = new Scanner(System.in);
+        int max;
+        int Array[];
+        Array = new int[max];
+        int i
+        int oddCount;
+        int evenCount;
+
+        System.out.println("enter the amount of numbers in the array -> ");
+        max = new.scanInt();
+        for(i = 0; i < Array.length; index++)
+        {
+        System.out.println("enter a number");
+        Array = new.scanInt
+        if(boo = false)
+        {
+            oddCount ++
+        }
+        if(boo = true)
+        {
+            evenCount ++
+        }
+        System.out.println("there are " + evenCount + " even and " + oddCount + " odd numbers");
+        }
+    }
+    static boolean isEven(int input)
+    {
+        boolean boo = false;
+        if(input % 2 ==0)
+                {
+                    boo = true;
+                }
+          return boo;
+    }
+}
